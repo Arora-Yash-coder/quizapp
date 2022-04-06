b382c68db95c943833edeed75bd12ac731914dc0
Yash Arora <yasharora9416@gmail.com>
2022-04-06 11:40:29 +0530

    Func: Added a way to add user reports

---
+++ lib/services/firestore.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // Gets us the topics collection
+  Future<List<Topic>> getTopics() async {
+    var ref = _db.collection('topics');
+    var snapshot = await ref.get();
+    var data = snapshot.docs.map((s) => s.data());
+    var topics = data.map((d) => Topic.fromJson(d));
+    return topics.toList();
+  }
+
+  // Gets an single quiz
+  Future<Quiz> getQuiz(String quizId) async {
+    var ref = _db.collection('quizzes').doc(quizId);
+    var snapshot = await ref.get();
+    return Quiz.fromJson(snapshot.data() ?? {});
+  }
+
+  // Gets the user's report to firebase
+  Stream<Report> streamReport() {
+    return AuthService().userStream.switchMap((user) {
+      if (user != null) {
+        var ref = _db.collection('reports').doc(user.uid);
+        return ref.snapshots().map((doc) => Report.fromJson(doc.data()!));
+      } else {
+        return Stream.fromIterable([Report()]);
+      }
+    });
+  }
+
+  // Reports the user's report to firebase
+  Future<void> updateUserReport(Quiz quiz) {
+    var user = AuthService().user!;
+    var ref = _db.collection('reports').doc(user.uid);
+
+    var data = {
+      'total': FieldValue.increment(1),
+      'topics': {
+        quiz.topic: FieldValue.arrayUnion([quiz.id])
+      }
+    };
+
+    return ref.set(data, SetOptions(merge: true));
+  }
+}
