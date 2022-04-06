bb3de2dedf5e7123172972fd50f1858fd7a46b08
Yash Arora <yasharora9416@gmail.com>
2022-04-06 23:05:26 +0530

    Added Ui For Profile page

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,20 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+
+import '../shared/shared.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ElevatedButton(
-        child: const Text('Sign Out'),
-        onPressed: () async {
-          await AuthService().signOut();
-          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-        },
-      ),
-    );
+    var report = Provider.of<Report>(context);
+    var user = AuthService().user;
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(user.displayName ?? 'Guest'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                margin: const EdgeInsets.only(top: 50),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(user.photoURL ??
+                        'https://www.gravatar.com/avatar/placeholder'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(user.email ?? '',
+                  style: Theme.of(context).textTheme.headline6),
+              const Spacer(),
+              Text('${report.total}',
+                  style: Theme.of(context).textTheme.headline2),
+              Text('Quizzes Completed',
+                  style: Theme.of(context).textTheme.subtitle2),
+              const Spacer(),
+              ElevatedButton(
+                child: const Text('Sign Out'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.deepOrange,
+                ),
+                onPressed: () async {
+                  await AuthService().signOut();
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/', (route) => false);
+                },
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Loader();
+    }
   }
 }
