0ea2a95bcfa1140ebb7b34380ec697632a60f810
Yash Arora <yasharora9416@gmail.com>
2022-04-06 17:56:00 +0530

    UI: Added basics of the Topics page

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:quizapp/services/firestore.dart';
 import 'package:quizapp/services/models.dart';
 import 'package:quizapp/shared/shared.dart';
+import 'package:quizapp/topics/topic_item.dart';
 
 import 'package:quizapp/services/auth.dart';
 
@@ -37,14 +39,26 @@ class TopicsPage extends StatelessWidget {
         } else if (snapshot.hasData) {
           var topics = snapshot.data!;
           return Scaffold(
-            appBar: AppBar(),
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text('Topics'),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    FontAwesomeIcons.userCircle,
+                    color: Colors.pink[200],
+                  ),
+                  onPressed: () => Navigator.pushNamed(context, '/profile'),
+                )
+              ],
+            ),
             bottomNavigationBar: const BottomNavBar(),
             body: GridView.count(
               primary: false,
               padding: const EdgeInsets.all(20.0),
               crossAxisSpacing: 10,
               crossAxisCount: 2,
-              children: topics.map((topic) => Text(topic.title)).toList(),
+              children: topics.map((topic) => TopicItem(topic: topic)).toList(),
             ),
           );
         } else {
